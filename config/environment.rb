41d22cb42a87145b88b29e6de4ab5084b426c29d
wael ali <waelali595@gmail.com>
2018-03-13 00:10:52 +0100

    add setting for mailer on production heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configuring heroku sendgrid mailing service (you can put this in production.rb)
+ActionMailer::Base.smtp_settings  = {
+    :address => 'smpt.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],# dont bother about, heroku provide them
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_startstls_auto => true
+}
